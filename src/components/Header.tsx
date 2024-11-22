3686857d20f743cebed0af9ca9fcd95a06e56385
Ashwin Pande <ashwin2201@yahoo.com>
2024-11-22 18:03:09 +0900

    first few commits

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className='h-16 bg-primary'>
+        <h1 className='text-2xl p-4'>My todo list</h1>
+    </header>
+  )
+}
+
+export default Header
